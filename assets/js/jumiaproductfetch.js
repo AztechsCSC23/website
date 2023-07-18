c943023edf2de2f69038b14a285364f0eb902cd5
Andrew Otule <98696691+andrewotule@users.noreply.github.com>
2023-07-18 10:24:00 +0300

    Create jumiaproductfetch.js

---
+++ assets/js/jumiaproductfetch.js
@@ -0,0 +1,32 @@
+function searchProducts() {
+    var searchQuery = document.getElementById("searchInput").value;
+  
+    // Make an HTTP GET request to the search API endpoint
+    fetch(`https://boiling-fjord-00108-52329a44ed31.herokuapp.com/search?query=${encodeURIComponent(searchQuery)}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json(); // Convert the response to an object
+        } else {
+          throw new Error("Error: " + response.status);
+        }
+      })
+      .then((responseData) => {
+        const objectData = responseData.collection2; // Access the desired collection  since this is jumia its switched to collection2
+  
+        let tableData = "";
+        objectData.forEach((values) => {
+          tableData += `<tr>
+            <td><a href="item.html?id=${values._id}">${values._id}</a></td>
+            <td>${values.name}</td>
+            <td>${values.price}</td>
+            <td><img src="${values.images}" alt="${values.name}"/></td>
+          </tr>`;
+        });
+  
+        document.getElementById("table_body").innerHTML = tableData;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  
